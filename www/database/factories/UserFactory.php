2f5c96f954a0299b5833e4ea1363a612574e0ca8
Tim van Boxtel <tim@vanboxtel.ca>
2018-01-22 23:28:02 +0000

    User factory available to create roles via states.
    Unit Test checks that logged in user can see their name

--- www/database/factories/UserFactory.php
+++ www/database/factories/UserFactory.php
@@ -12,14 +12,23 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
+$role_user = App\Role::where('slug', 'user')->first();
 
 $factory->define(App\User::class, function (Faker $faker) {
     static $password;
-
+    
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'remember_token' => str_random(10)
     ];
 });
+
+$factory->state(App\User::class, 'admin', [
+	'role_id' => App\Role::where('slug', 'admin')->first()->id
+]);
+
+$factory->state(App\User::class, 'user', [
+	'role_id' => $role_user->id
+]);
--- www/tests/Feature/IndexTest.php
+++ www/tests/Feature/IndexTest.php
@@ -5,17 +5,26 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class IndexTest extends TestCase
 {
+	
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testIndexOK()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testLoggedIn()
+    {
+    	$user = factory(\App\User::class)->states('user')->create();
+    	$this->actingAs($user)
+    		->get('/')
+    		->assertSee($user->name);	
     }
 }
